314a86f8a8b4efdb1ae4c96364238e481aa4893f
ocean <oceanwangyang@163.com>
2021-10-07 11:59:51 +0800

    fixed run on mac

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -6,4 +6,41 @@ pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {:?} toast please", meal);
+    println!("{:?}", back_of_house::Appetizer::Soup);
+    println!("{:?}", back_of_house::Appetizer::Salad);
+}
+// #[cfg(test)]
+// mod tests {
+//     use crate::eat_at_restaurant;
+
+//     #[test]
+//     fn it_works() {
+//         eat_at_restaurant();
+//     }
+// }
+mod back_of_house {
+    #[derive(Debug)]
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
 }
--- restaurant/src/main.rs
+++ restaurant/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
+    let mut v: Vec<i32> = Vec::new();
+    v.push(5);
     restaurant::eat_at_restaurant();
 }
